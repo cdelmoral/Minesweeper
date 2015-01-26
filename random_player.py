0160709cf6e059de8b64ff2b5e28f32d3f38fdaf
Carlos del Moral <cdelmoral@chi-en-n8f1g3.trustwave.com>
2015-01-26 11:13:19 -0600

    Create RandomPlayer class.

--- random_player.py
+++ random_player.py
@@ -1,27 +1,33 @@
 import minesweeper as ms
 import random
 
-width = 10
-height = 10
+class RandomPlayer(object):
 
-games_lost = 0
-games_won = 0
+	def __init__(self, height, width, num_mines, tries):
+		self.width = width
+		self.height = height
+		self.tries = tries
+		self.num_mines = num_mines
 
-for x in range(100):
-	board = ms.Board(10, 10, 5)
-	while board.remaining_cells > 0:
-		col = random.randrange(0, width)
-		row = random.randrange(0, height)
-		if (board.user_board[col][row] == ms.UNDISCOVERED):
-			if board.discover_cell(col, row):
-				print "Game lost: there were %d cells remaining." % board.remaining_cells
-				games_lost += 1
-				break
+	def play(self):
+		games_lost = 0
+		games_won = 0
 
-	if board.remaining_cells == 0:
-		games_won += 1
-		print "Game won."
+		for x in range(self.tries):
+			board = ms.Board(self.height, self.width, self.num_mines)
+			while board.remaining_cells > 0:
+				col = random.randrange(0, self.width)
+				row = random.randrange(0, self.height)
+				if (board.user_board[col][row] == ms.UNDISCOVERED):
+					if board.discover_cell(col, row):
+						print "Game lost: there were %d cells remaining." % board.remaining_cells
+						games_lost += 1
+						break
 
-print
-print "Total games lost: %d." % games_lost
-print "Total games won: %d." % games_won
+			if board.remaining_cells == 0:
+				games_won += 1
+				print "Game won."
+
+		print
+		print "Total games lost: %d." % games_lost
+		print "Total games won: %d." % games_won
